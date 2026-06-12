2f4835b6f687d903d8c6d14365f7975665a8b4eb
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-06-12 17:49:54 +0530

    Tuples and List Difference Explanation

---
+++ tuples.py
@@ -0,0 +1,28 @@
+# Tuples are very similar to list but with one difference, you cant modify a tuple
+# Immutable
+
+# Explaining the difference
+
+# Mutable - Lists - use these if you want modification
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+
+print(list_1)
+print(list_2)
+
+list_1[0] = 'Art'
+
+print(list_1)
+print(list_2)
+
+# Immutable - Tuples - use these if you don't want modification
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+# tuple_1[0] = 'Art' # TypeError - 'tuple' object does not support item assignment
+
+# print(tuple_1)
+# print(tuple_2)
